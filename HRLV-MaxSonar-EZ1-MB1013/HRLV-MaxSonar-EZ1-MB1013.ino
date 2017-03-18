adc8627eee34d805144688f6ecbad308e351209d
Arduino <aportante10@gmail.com>
2017-03-18 11:22:40 -0700

    Basic demo code to read from HRLV-MaxSonar-EZ1 ultrasonic

---
+++ HRLV-MaxSonar-EZ1-MB1013/HRLV-MaxSonar-EZ1-MB1013.ino
@@ -0,0 +1,47 @@
+#include <Wire.h>
+//Written to read sensor data through PWM
+//Arduino Uno
+
+
+const int SensorRecvPin = 3; //PWM pin
+long sensor;       //Raw sensor data
+long mm;           //conversion to mm
+long inches;       //conversion to inches 
+
+void ReadSensor(){
+  //Get the reading from the sesnsor in raw form(uS)
+  //1uS = 1mm
+  
+  sensor = pulseIn(SensorRecvPin, HIGH); 
+  mm = sensor;
+  
+  inches = mm/25.4; // convert to inches according to datasheet
+}
+
+void PrintRange(){
+  Serial.print("Distance =");
+  Serial.print(mm);
+  Serial.print(" mm /  ");
+  Serial.print(inches);
+  Serial.print(" inches");
+  Serial.println();
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  ReadSensor();
+  PrintRange();
+  
+  //VERY IMPORTANT!!!
+  //HRLV-MaxSonar-EZ1 has a sensor read rate of 10Hz
+  //Delay required to match!
+  delay(100);
+
+}
+
+
